a78758c1667f8fb486c99bd97e43bac1c99bbb13
Mouad Garroud <mouadgarroud@gmail.com>
2024-12-27 21:45:45 +0100

    speed_wifi.py

--- speed_wifi.py
+++ speed_wifi.py
@@ -1,8 +1,7 @@
+# Mouad Garroud
 import speedtest
 test = speedtest.Speedtest()
 download = test.download() / 1_000_000
 upload = test.upload() / 1_000_000
 print(f"Download speed: {download:.2f} Mbps")
 print(f"Upload speed: {upload:.2f} Mbps")
-
-#Mouad Garroud
